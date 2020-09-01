91dbfb1a45d020c8c05c5f884bd05ded488c7571
chengqain <chengqian@szy.cn>
2020-09-01 21:12:09 +0800

    添加 FBRetainCycleDetector

--- CQProject/CQProject/iOS开发高手课/fishhook/CQFishHookViewController+FishHook.m
+++ CQProject/CQProject/iOS开发高手课/fishhook/CQFishHookViewController+FishHook.m
@@ -13,29 +13,43 @@
 @implementation CQFishHookViewController (FishHook)
 +(void)load {
     struct rebinding ex;
+    // 需要hook的函数名 C字符串
     ex.name = "method_exchangeImplementations";
+    // 新的函数地址
     ex.replacement = myExchange;
-    ex.replaced = (void *)exhangeP;
+    // 指向原函数地址
+    ex.replaced = (void *)&exhangeP;
+    // rebinding结构体数组
     struct rebinding rebs[1] = {ex};
-    rebind_symbols(rebs, 1);
+    int isbind = rebind_symbols(rebs, 1);
+    if (isbind == 0) {
+        NSLog(@"绑定成功");
+    } else {
+        NSLog(@"绑定失败");
+    }
 }
 
 void (*exhangeP)(Method _Nonnull m1,Method _Nonnull m2);
 
 void myExchange(Method _Nonnull m1,Method _Nonnull m2) {
+    // hookNSlog
     struct rebinding nslog;
     nslog.name = "NSLog";
+    // 新的函数地址
     nslog.replacement = myNslog;
+    // 指向原来的函数地址
     nslog.replaced = (void *)&sys_nslog;
     struct rebinding rebs[1] = {nslog};
     rebind_symbols(rebs, 1);
     NSLog(@"想搞事情");
 }
 
-static void(*sys_nslog)(NSString * format,...);
+void(*sys_nslog)(NSString * format,...);
+
 
 void myNslog(NSString * format,...) {
     format = [format stringByAppendingString:@"发现了非法操作\n"];
+    // 调用原来系统的NSLog方法
     sys_nslog(format);
 }
 @end
