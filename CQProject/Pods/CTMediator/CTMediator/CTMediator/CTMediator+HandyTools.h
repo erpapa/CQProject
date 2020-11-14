6dacc825fe1a0352d75e5141badfe2407426ef26
chengqian <chartype@163.com>
2020-11-15 01:37:57 +0800

    新PC项目修改提交

--- CQProject/Pods/CTMediator/CTMediator/CTMediator/CTMediator+HandyTools.h
+++ CQProject/Pods/CTMediator/CTMediator/CTMediator/CTMediator+HandyTools.h
@@ -6,7 +6,10 @@
 //  Copyright © 2020 casa. All rights reserved.
 //
 
+#if TARGET_OS_IOS
+
 #import "CTMediator.h"
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -19,3 +22,5 @@ NS_ASSUME_NONNULL_BEGIN
 @end
 
 NS_ASSUME_NONNULL_END
+
+#endif
